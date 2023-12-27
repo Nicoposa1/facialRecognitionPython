e3386e99f5a8d70c6f93e30ba4479189cc1dcc52
Nicolas Posa <nicoposa57@gmail.com>
2023-12-27 11:41:02 -0300

    face counter

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import cv2
 
-# Path: main.py
-
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 cap = cv2.VideoCapture(0)
 
+unique_faces = set()  # Conjunto para almacenar caras únicas
 
 while True:
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 3)
+        unique_faces.add((x, y, x+w, y+h))  # Agregar coordenadas únicas al conjunto
+
+    # Mostrar el número de caras únicas
+    num_unique_faces = len(unique_faces)
+    cv2.putText(img, f'Caras Unicas: {num_unique_faces}', (10, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
     cv2.imshow('img', img)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
 
 cap.release()
+cv2.destroyAllWindows()
